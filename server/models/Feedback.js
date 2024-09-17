7e12a4a640feb4b6104fc0d68850f17063606b0c
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-18 01:19:10 +0530

    feedback model implemented

---
+++ server/models/Feedback.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+    userName : { type: String, required: true },
+    email: { type: String, required: true},
+    feedbackText: { type: String, required: true },
+    rating: { type:Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+    status: { type: String, default: 'pending', enum: ['rejected', 'pending', 'approved']}
+});
+
+export default mongoose.model("Feedback",feedbackSchema);
