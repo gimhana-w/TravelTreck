7b237f77182b7ef1c5e774c9b8cdaf774e2e2aa0
Navindu Nimsara <navidunimsara@gmail.com>
2024-09-18 01:54:27 +0530

    minor typo changes

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ mongoose
   .then(() => {
     console.log("Database connected");
     app.listen(5000, () => {
-      console.log(`Server is running on prot 5000`);
+      console.log(`Server is running on port 5000`);
       
     });
   })
