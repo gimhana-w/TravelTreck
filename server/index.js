fb8b80c47b281dba0cb99bf1b2c5cb6d98eb07c9
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-09-21 13:42:00 +0530

    ADD:finance crud done

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import packageRoutes from "./route/packageRoutes.js";
 import advertisementRoutes from "./route/advertisementRoutes.js";
 import reviewRoutes from "./route/reviewRoutes.js";
 import adventureRoutes from "./route/adventureRoutes.js";
+import financeRoutes from "./route/financeRoute.js";
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use("/api/packages", packageRoutes);
 app.use("/api/advertisements", advertisementRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/adventures", adventureRoutes);
+app.use("/api/finance", financeRoutes);
 //database conection
 mongoose
   .connect("mongodb+srv://admin:WNM6le3U5gTaDz5d@users.xzqkmsk.mongodb.net/")
